9bf96827189da16ce6464eb08aceb9a4162b90b1
IrinaPeshko <privet-kira1999@mail.ru>
2023-04-14 01:55:50 +0300

    fix  what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require("../extensions/index.js");
-
 /**
  * Extract season from given date and expose the enemy scout!
  *
@@ -11,26 +9,29 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
+
 function getSeason(date) {
-  if (!arguments.length) {
+  if (arguments.length == 0) {
     return "Unable to determine the time of year!";
   }
   if (!Date.parse(date.toString())) {
-    return "Invalid date!";
+    throw new Error("Invalid date!");
+  }
+  if (!date instanceof Date) {
+    throw new Error("Invalid date!");
   }
   const month = date.getMonth();
-
-  if (month === 11 || month === 0 || month === 1) {
+  if (month == 11 || month == 0 || month == 1) {
     return "winter";
-  } else if (month === 2 || month === 3 || month === 4) {
+  } else if (month == 2 || month == 3 || month == 4) {
     return "spring";
-  } else if (month === 5 || month === 6 || month === 7) {
+  } else if (month == 5 || month == 6 || month == 7) {
     return "summer";
-  } else if (month === 8 || month === 9 || month === 10) {
+  } else if (month == 8 || month == 9 || month == 10) {
     return "autumn";
   } else {
-    return "Invalid date!";
+    throw new Error("Invalid date!");
   }
 }
 
-console.log(getSeason(new Date(2020, 02, 31)));
+console.log(getSeason(new Date(1994, 8, 26, 3, 0, 11, 500)));
