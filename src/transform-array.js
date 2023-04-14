ad3a13833732e5fc6006e4e9b825984500ef4b12
IrinaPeshko <privet-kira1999@mail.ru>
2023-04-14 23:40:42 +0300

    transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,8 +13,51 @@ const { NotImplementedError } = require("../extensions/index.js");
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(arr) {}
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const cloneArr = arr.slice();
+  for (let i = 0; i < cloneArr.length; i++) {
+    if (cloneArr[i] === "--double-next") {
+      if (cloneArr[i + 1]) {
+        cloneArr[i] = cloneArr[i + 1];
+      } else {
+        cloneArr.splice(cloneArr[i - 1], 1);
+      }
+    } else if (cloneArr[i] === "--double-prev") {
+      if (cloneArr[i - 1]) {
+        cloneArr[i] = cloneArr[i - 1];
+      } else {
+        cloneArr.splice(cloneArr[i - 1], 1);
+      }
+    } else if (cloneArr[i] === "--discard-next") {
+      if (cloneArr[i + 1]) {
+        if (
+          cloneArr[i + 2] === "--discard-prev" ||
+          cloneArr[i + 2] === "--double-prev"
+        ) {
+          cloneArr.splice(cloneArr[i - 1], 3);
+        } else {
+          cloneArr.splice(cloneArr[i - 1], 2);
+        }
+      } else {
+        cloneArr.splice(cloneArr[i - 1], 1);
+      }
+    } else if (cloneArr[i] === "--discard-prev") {
+      if (cloneArr[i - 1]) {
+        cloneArr.splice([i - 1], 2);
+      } else {
+        cloneArr.splice(cloneArr[i - 1], 1);
+      }
+    }
+  }
+  return cloneArr;
+}
 
+console.log(
+  transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5])
+);
 module.exports = {
   transform,
 };
