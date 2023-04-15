8e2cbc32725484e071fa8d1cf6ef6379cd1d9fef
IrinaPeshko <privet-kira1999@mail.ru>
2023-04-15 14:22:11 +0300

    simple-chain first part

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,60 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.splite("~~").length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (arguments.length == 0) {
+      this.arr.push(`( )`);
+    } else {
+      this.arr.push(`( ${value} )`);
+    }
+    // console.log(this.arr);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      !Number.isInteger(position) ||
+      position <= 0 ||
+      position >= this.arr.length
+    ) {
+      throw new Error("You can't remove incorrect link!");
+    } else {
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    return this.arr.join("~~");
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
+
+console.log(
+  chainMaker
+    .reverseChain()
+    .reverseChain()
+    .reverseChain()
+    .addLink(NaN)
+    .reverseChain()
+    .addLink(null)
+    .addLink(1.233)
+    .addLink(true)
+    .addLink(false)
+    .removeLink(3)
+    .addLink(1.233)
+    .finishChain()
+);
