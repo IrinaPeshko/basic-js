9959dbb994ade00344b16d6a9a5ea2b703d72fd6
IrinaPeshko <privet-kira1999@mail.ru>
2023-04-18 02:09:52 +0300

    encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const result = [];
+  arrStr = str.split("");
+  for (let i = 0; i < arrStr.length; i++) {
+    let count = 1;
+    function findTheSame() {
+      if (arrStr[i] === arrStr[i + 1]) {
+        count++;
+        i++;
+        findTheSame();
+      } else {
+        if (count === 1) {
+          result.push(arrStr[i]);
+        } else {
+          result.push(count, arrStr[i]);
+        }
+      }
+    }
+    findTheSame();
+  }
+  return result.join("");
 }
 
+console.log(encodeLine("aabbbc"));
+
 module.exports = {
-  encodeLine
+  encodeLine,
 };
